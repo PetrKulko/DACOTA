0e0749fc795828e74b90f146d7e5b03751c6424e
Petr_Kulko <petrokulko@gmail.com>
2021-04-27 21:08:01 +0300

    compleat desctop version

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$('.comments__items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    nextArrow: '<img src="../img/arrow-right.svg" alt="arrow">',
+    prevArrow: '<img src="../img/arrow.svg" alt="arrow">',
+});
