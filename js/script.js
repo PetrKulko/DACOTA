151086ff83b049bf85cf474e143702281d9f3599
Petr_Kulko <petrokulko@gmail.com>
2021-04-28 14:22:49 +0300

    adaptiv mobile

--- js/script.js
+++ js/script.js
@@ -5,4 +5,17 @@ $('.comments__items').slick({
     autoplay: true,
     nextArrow: '<img src="../img/arrow-right.svg" alt="arrow">',
     prevArrow: '<img src="../img/arrow.svg" alt="arrow">',
+    responsive: [
+        {
+            breakpoint: 1310,
+            settings: {
+                slidesToShow: 2
+            },
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
 });
+
